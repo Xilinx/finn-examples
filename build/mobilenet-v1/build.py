ad9f1b552ce74ba499c375c6b5539fa03b75fd6b
Felix Jentzsch <fepaje@mail.upb.de>
2021-03-09 18:04:45 +0100

    Add ZCU102/104 support to build script

--- build/mobilenet-v1/build.py
+++ build/mobilenet-v1/build.py
@@ -28,6 +28,9 @@
 
 import finn.builder.build_dataflow as build
 import finn.builder.build_dataflow_config as build_cfg
+from finn.util.basic import alveo_default_platform
+import os
+import shutil
 
 # custom steps for mobilenetv1
 from custom_steps import (
@@ -37,9 +40,30 @@ from custom_steps import (
 )
 
 model_name = "mobilenetv1-w4a4"
-board = "U250"
-vitis_platform = "xilinx_u250_xdma_201830_2"
-synth_clk_period_ns = 3.0
+
+# which platforms to build the networks for
+zynq_platforms = ["ZCU102", "ZCU104"]
+alveo_platforms = ["U250"]
+platforms_to_build = zynq_platforms + alveo_platforms
+
+# determine which shell flow to use for a given platform
+def platform_to_shell(platform):
+    if platform in zynq_platforms:
+        return build_cfg.ShellFlowType.VIVADO_ZYNQ
+    elif platform in alveo_platforms:
+        return build_cfg.ShellFlowType.VITIS_ALVEO
+    else:
+        raise Exception("Unknown platform, can't determine ShellFlowType")
+
+# select target clock frequency
+def platform_to_clk_period(platform):
+    if platform in zynq_platforms:
+        return 10.0
+    elif platform in alveo_platforms:
+        return 3.0
+
+# create a release dir, used for finn-examples release packaging
+os.makedirs("release", exist_ok=True)
 
 mobilenet_build_steps = [
     step_mobilenet_streamline,
@@ -48,6 +72,7 @@ mobilenet_build_steps = [
     "step_create_dataflow_partition",
     "step_apply_folding_config",
     "step_generate_estimate_reports",
+    "step_hls_codegen",
     "step_hls_ipgen",
     "step_set_fifo_depths",
     "step_create_stitched_ip",
@@ -56,25 +81,67 @@ mobilenet_build_steps = [
     "step_deployment_package",
 ]
 
+for platform_name in platforms_to_build:
+    shell_flow_type = platform_to_shell(platform_name)
+    if shell_flow_type == build_cfg.ShellFlowType.VITIS_ALVEO:
+        vitis_platform = alveo_default_platform[platform_name]
+        # for Alveo, use the Vitis platform name as the release name
+        # e.g. xilinx_u250_xdma_201830_2
+        release_platform_name = vitis_platform
+    else:
+        vitis_platform = None
+        # for Zynq, use the board name as the release name
+        # e.g. ZCU104
+        release_platform_name = platform_name
+    platform_dir = "release/%s" % release_platform_name
+    os.makedirs(platform_dir, exist_ok=True)
 
-cfg = build_cfg.DataflowBuildConfig(
-    steps=mobilenet_build_steps,
-    output_dir="output_%s_%s" % (model_name, board),
-    folding_config_file="folding_config/%s_folding_config.json" % board,
-    synth_clk_period_ns=synth_clk_period_ns,
-    board=board,
-    shell_flow_type=build_cfg.ShellFlowType.VITIS_ALVEO,
-    # folding config comes with FIFO depths already
-    auto_fifo_depths=False,
-    vitis_platform=vitis_platform,
-    # enable extra performance optimizations (physopt)
-    vitis_opt_strategy=build_cfg.VitisOptStrategyCfg.PERFORMANCE_BEST,
-    generate_outputs=[
-        build_cfg.DataflowOutputType.PYNQ_DRIVER,
-        build_cfg.DataflowOutputType.ESTIMATE_REPORTS,
-        build_cfg.DataflowOutputType.BITFILE,
-        build_cfg.DataflowOutputType.DEPLOYMENT_PACKAGE,
-    ],
-)
-model_file = "models/%s_pre_post_tidy.onnx" % model_name
-build.build_dataflow_cfg(model_file, cfg)
+    cfg = build_cfg.DataflowBuildConfig(
+        steps=mobilenet_build_steps,
+        output_dir="output_%s_%s" % (model_name, release_platform_name),
+        folding_config_file="folding_config/%s_folding_config.json" % platform_name,
+        synth_clk_period_ns=platform_to_clk_period(platform_name),
+        board=platform_name,
+        shell_flow_type=shell_flow_type,
+        vitis_platform=vitis_platform,
+        # folding config comes with FIFO depths already
+        auto_fifo_depths=False,
+        # enable extra performance optimizations (physopt)
+        vitis_opt_strategy=build_cfg.VitisOptStrategyCfg.PERFORMANCE_BEST,
+        generate_outputs=[
+            build_cfg.DataflowOutputType.PYNQ_DRIVER,
+            build_cfg.DataflowOutputType.ESTIMATE_REPORTS,
+            build_cfg.DataflowOutputType.BITFILE,
+            build_cfg.DataflowOutputType.DEPLOYMENT_PACKAGE,
+        ],
+    )
+    model_file = "models/%s_pre_post_tidy.onnx" % model_name
+    build.build_dataflow_cfg(model_file, cfg)
+
+    # copy bitfiles and runtime weights into release dir if found
+    bitfile_gen_dir = cfg.output_dir + "/bitfile"
+    files_to_check_and_copy = [
+        "finn-accel.bit",
+        "finn-accel.hwh",
+        "finn-accel.xclbin",
+    ]
+    for f in files_to_check_and_copy:
+        src_file = bitfile_gen_dir + "/" + f
+        dst_file = platform_dir + "/" + f.replace("finn-accel", model_name)
+        if os.path.isfile(src_file):
+            shutil.copy(src_file, dst_file)
+
+    weight_gen_dir = cfg.output_dir + "/driver/runtime_weights"
+    weight_dst_dir = platform_dir + "/%s_runtime_weights" % model_name
+    if os.path.isdir(weight_gen_dir):
+        weight_files = os.listdir(weight_gen_dir)
+        if weight_files:
+            shutil.copytree(weight_gen_dir, weight_dst_dir)
+
+    # create zipfile for all examples for this platform
+    shutil.make_archive(
+        "release/" + release_platform_name,
+        "zip",
+        root_dir="release",
+        base_dir=release_platform_name,
+    )
