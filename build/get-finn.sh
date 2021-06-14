5c4fb71f170260ee7a1e83df75871a1b76f34a19
Lucian Petrica <lucianp@xilinx.com>
2021-06-14 17:06:49 +0100

    Updated finn to commit with resource estimate fix

--- build/get-finn.sh
+++ build/get-finn.sh
@@ -30,7 +30,7 @@
 # URL for git repo to be cloned
 REPO_URL=https://github.com/Xilinx/finn
 # commit hash for repo
-REPO_COMMIT=e5da788bdc74fc9c234bb0176521ad51e830c22e
+REPO_COMMIT=c8be5048a7f1647f7c72be7c7cd158e851d47a86
 # directory (under the same folder as this script) to clone to
 REPO_DIR=finn
 
