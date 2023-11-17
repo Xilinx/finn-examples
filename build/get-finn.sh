ac69a5766701eaa950be41798c30edd9f90996df
auphelia <jakobapk@web.de>
2023-11-17 11:15:24 +0000

    Update finn commit

--- build/get-finn.sh
+++ build/get-finn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Copyright (c) 2020, Xilinx
+# Copyright (C) 2020-2022, Xilinx
+# Copyright (C) 2023, Advanced Micro Devices, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,7 +31,7 @@
 # URL for git repo to be cloned
 REPO_URL=https://github.com/Xilinx/finn
 # commit hash for repo
-REPO_COMMIT=0484acba8e28dab95380bd78466c3d4230bf9960
+REPO_COMMIT=297fa3dbaa327d4eeeb26f789e5312a8dd3511f4
 # directory (under the same folder as this script) to clone to
 REPO_DIR=finn
 
