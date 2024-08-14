8baeb395d70b716a2a406dd36cb0e45e483f08ae
auphelia <jakobapk@web.de>
2024-08-14 17:18:58 +0100

    Upgrade numpy dependency

--- setup.py
+++ setup.py
@@ -120,7 +120,7 @@ setup(
     install_requires=[
         "pynq>=2.5.1",
         "bitstring>=3.1.7",
-        "numpy==1.21.5",
+        "numpy==1.24.1",
         "finn-dataset_loading==0.0.5",  # noqa
     ],
     extras_require={
